129967c5cf04be4042d90bd13b97ba12c5f2cbd0
IfedayoPeter <awoniyiifedayopeter@gmail.com>
2023-08-17 02:53:09 +0100

    Task Tracking API with Authentication

--- src/main/java/com/project/TaskTrackingAPI/model/Task.java
+++ src/main/java/com/project/TaskTrackingAPI/model/Task.java
@@ -15,7 +15,7 @@ import java.time.LocalDate;
 public class Task {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "ID")
     private Long ID;
     private String title;
